360da666580edfe1ac6bd4c5dff352b8037c243a
Mattias007 <mattiasmaasik@ametikool.ee>
2023-05-03 12:44:07 +0300

    2

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -6,6 +6,8 @@
     <div class="gallery">
         @foreach ($category->galleryImages as $image)
             <img src="{{ asset('storage/images/' . $image->filename) }}" alt="{{ $image->filename }}">
+            {{ asset('storage/images/' . $image->filename) }}
         @endforeach
     </div>
 @endforeach
+
